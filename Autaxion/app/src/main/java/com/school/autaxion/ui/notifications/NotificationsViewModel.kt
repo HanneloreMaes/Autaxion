580ce931c30e46447f11b08404288d191bf05c34
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-04-24 20:45:31 +0200

    Project naar repo

---
+++ Autaxion/app/src/main/java/com/school/autaxion/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.school.autaxion.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
