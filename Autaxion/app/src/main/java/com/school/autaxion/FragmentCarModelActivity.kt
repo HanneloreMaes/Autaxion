2d5ed8cf4b9791feecb07179591c0f8a33641a44
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-05-04 14:51:23 +0200

    Extra activity voor modellen auto's

---
+++ Autaxion/app/src/main/java/com/school/autaxion/FragmentCarModelActivity.kt
@@ -0,0 +1,11 @@
+package com.school.autaxion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FragmentCarModelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_car_model)
+    }
+}
