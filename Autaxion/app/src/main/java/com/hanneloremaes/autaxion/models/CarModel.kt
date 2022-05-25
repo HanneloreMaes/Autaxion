2e897c0792bd9d08f97539278d3d3223318ff4db
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-05-25 14:25:14 +0200

    Model voor car

--- Autaxion/app/src/main/java/com/hanneloremaes/autaxion/models/CarModel.kt
+++ Autaxion/app/src/main/java/com/hanneloremaes/autaxion/models/CarModel.kt
@@ -1,4 +1,28 @@
 package com.hanneloremaes.autaxion.models
 
-class CarModel {
+class CarModel (
+    var brand: String = "",
+    var carimg: String = " "
+){
+
+    fun CarsModal(carBrand: String, carImage: String) {
+        this.brand = carBrand;
+        this.carimg = carImage;
+    }
+
+
+    fun getBrandCar(): String{
+        return brand;
+    }
+    fun setBrandCar(brandCar: String) {
+        this.brand = brandCar;
+    }
+
+    fun getCarImg(): String {
+        return carimg
+    }
+
+    fun setCarImg(imgCar: String) {
+        carimg = imgCar
+    }
 }
