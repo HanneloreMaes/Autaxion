51333bfa5ccdc7b774bfa0ad6d6c3684efc95b9e
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-05-27 19:36:21 +0200

    OnItemClickEvent voor Brand recycler

--- Autaxion/app/src/main/java/com/hanneloremaes/autaxion/model/CarAdapter.kt
+++ Autaxion/app/src/main/java/com/hanneloremaes/autaxion/model/CarAdapter.kt
@@ -11,10 +11,24 @@ import androidx.recyclerview.widget.RecyclerView
 import com.hanneloremaes.autaxion.R
 import kotlinx.android.synthetic.main.fragment_car_card.view.*
 
-class CarAdapter(private val cars: List<Car>) : RecyclerView.Adapter<CarAdapter.ViewHolder>(){
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+class CarAdapter(private val cars: List<Car>, private val listener: OnItemClickListener) : RecyclerView.Adapter<CarAdapter.ViewHolder>() {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
         val brandCar: TextView = itemView.brandCar
-        val btnCar: Button = itemView.btnModelCar
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            val position: Int = adapterPosition
+            if(position != RecyclerView.NO_POSITION){
+                listener.onItemClick(position)
+            }
+        }
+    }
+
+    interface OnItemClickListener{
+        fun onItemClick(position: Int)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -25,10 +39,6 @@ class CarAdapter(private val cars: List<Car>) : RecyclerView.Adapter<CarAdapter.
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val car = cars[position]
         holder.brandCar.text = car.name.uppercase()
-
-        holder.btnCar.setOnClickListener {
-            Log.d("Hannelore", "You clicked me")
-        }
     }
 
     override fun getItemCount() = cars.size
