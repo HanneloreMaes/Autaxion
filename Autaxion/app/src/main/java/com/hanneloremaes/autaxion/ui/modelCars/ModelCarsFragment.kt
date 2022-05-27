6941ee89fe5abeb3f51fc5938afd63bd66693211
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-05-27 19:56:05 +0200

    OnItemClick function voor ModelCarFragment

--- Autaxion/app/src/main/java/com/hanneloremaes/autaxion/ui/modelCars/ModelCarsFragment.kt
+++ Autaxion/app/src/main/java/com/hanneloremaes/autaxion/ui/modelCars/ModelCarsFragment.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -17,14 +20,13 @@ import com.google.gson.Gson
 import com.hanneloremaes.autaxion.R
 import com.hanneloremaes.autaxion.databinding.FragmentDashboardBinding
 import com.hanneloremaes.autaxion.databinding.FragmentModelCarsBinding
-import com.hanneloremaes.autaxion.model.Car
-import com.hanneloremaes.autaxion.model.CarAdapter
-import com.hanneloremaes.autaxion.model.ModelCar
-import com.hanneloremaes.autaxion.model.ModelCarAdapter
+import com.hanneloremaes.autaxion.model.*
 import com.hanneloremaes.autaxion.ui.dashboard.DashboardViewModel
+import com.hanneloremaes.autaxion.ui.detailCar.CarDetailFragment
+import kotlinx.android.synthetic.main.fragment_dashboard.*
 import kotlinx.android.synthetic.main.fragment_model_card.*
 
-class ModelCarsFragment : Fragment() {
+class ModelCarsFragment : Fragment(), ModelCarAdapter.OnItemClickListener {
 
     var carsModelsList: MutableList<ModelCar> = mutableListOf()
     private var _binding: FragmentModelCarsBinding? = null
@@ -44,7 +46,7 @@ class ModelCarsFragment : Fragment() {
         val queue = Volley.newRequestQueue(this.context)
         val url = "https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformake/chrysler?format=json"
 
-        val carRequest = JsonObjectRequest(
+        val modelRequest = JsonObjectRequest(
             Request.Method.GET, url, null, { response ->
                 val results = response.getJSONArray("Results")
                 for (car in 0..(results.length()-1)){
@@ -57,13 +59,23 @@ class ModelCarsFragment : Fragment() {
 
                 val recyclerView: RecyclerView = binding.recyclerView
                 recyclerView.layoutManager = LinearLayoutManager(this.context)
-                recyclerView.adapter = ModelCarAdapter(carsModelsList)
+                recyclerView.adapter = ModelCarAdapter(carsModelsList, this)
             }, { Log.d("User", "Something went wrong") })
 
-        queue.add(carRequest)
+        queue.add(modelRequest)
         /*https://www.youtube.com/watch?v=e3MDW87mbR8 By SmallAcademy Pt. 1-3 eind*/
 
         return root
+    }
+
+    override fun onItemClick(position: Int) {
+        /*https://stackoverflow.com/questions/7793576/switching-between-fragment-view By Mats Hofman begin*/
+        val fragment: Fragment = CarDetailFragment()
+        val fm: FragmentManager = parentFragmentManager
+        val trans: FragmentTransaction = fm.beginTransaction()
+        trans.replace(R.id.recModel, fragment).commit()
+        /*https://stackoverflow.com/questions/7793576/switching-between-fragment-view By Mats Hofman eind*/
+
     }
 
     override fun onDestroyView() {
