f2282a944e2fd4ef302138fbfc54331af305756f
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-05-28 20:20:23 +0200

    Aanmaken nieuwe activity

---
+++ Autaxion/app/src/main/java/com/hanneloremaes/autaxion/HomeDetailCar.kt
@@ -0,0 +1,11 @@
+package com.hanneloremaes.autaxion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeDetailCar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_detail_car)
+    }
+}
