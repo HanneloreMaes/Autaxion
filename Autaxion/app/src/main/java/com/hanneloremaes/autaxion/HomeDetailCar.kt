f34f666d19363f20086d37fe0ca41be6910ee0a3
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-05-28 23:51:12 +0200

    OnClickListener voor back button -> HomeDetailCar

--- Autaxion/app/src/main/java/com/hanneloremaes/autaxion/HomeDetailCar.kt
+++ Autaxion/app/src/main/java/com/hanneloremaes/autaxion/HomeDetailCar.kt
@@ -2,10 +2,26 @@ package com.hanneloremaes.autaxion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.fragment.app.Fragment
+import com.hanneloremaes.autaxion.ui.home.HomeFragment
 
 class HomeDetailCar : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_detail_car)
+
+        val btnBack: ImageButton = findViewById(R.id.backBtn)
+        btnBack.setOnClickListener {
+            val homeFragment= HomeFragment()
+            val fragment: Fragment? = supportFragmentManager.findFragmentByTag(HomeFragment::class.java.simpleName)
+
+            if (fragment !is HomeFragment){
+                supportFragmentManager.beginTransaction().add(R.id.homeFragmentContainer, homeFragment, HomeFragment::class.java.simpleName).commit()
+            }
+            btnBack.visibility = View.GONE
+        }
     }
 }
