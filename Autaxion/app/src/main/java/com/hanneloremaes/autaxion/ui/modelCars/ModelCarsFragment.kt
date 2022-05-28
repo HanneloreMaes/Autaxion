8d2a6b1c218d73307cbedb27e0cc89616b90376f
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-05-28 16:30:38 +0200

    Ophalen met goede url

--- Autaxion/app/src/main/java/com/hanneloremaes/autaxion/ui/modelCars/ModelCarsFragment.kt
+++ Autaxion/app/src/main/java/com/hanneloremaes/autaxion/ui/modelCars/ModelCarsFragment.kt
@@ -10,7 +10,10 @@ import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.AuthFailureError
 import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.google.gson.Gson
@@ -18,10 +21,12 @@ import com.hanneloremaes.autaxion.R
 import com.hanneloremaes.autaxion.databinding.FragmentModelCarsBinding
 import com.hanneloremaes.autaxion.model.*
 import com.hanneloremaes.autaxion.ui.modelYearCars.ModelYearCarsFragment
+import org.json.JSONObject
 
 class ModelCarsFragment : Fragment(), ModelCarAdapter.OnItemClickListener {
     private val ARG_NAME = "argName"
     private val ARG_MODEL = "argModel"
+    private val api_key: String = "9gLyMNOM1kYEpyMKkLpoAg==ERVCQ4Gl57qfMsEK"
     var carsModelsList: MutableList<ModelCar> = mutableListOf()
     private var _binding: FragmentModelCarsBinding? = null
     private val binding get() = _binding!!
@@ -41,30 +46,32 @@ class ModelCarsFragment : Fragment(), ModelCarAdapter.OnItemClickListener {
 
         /*https://www.youtube.com/watch?v=e3MDW87mbR8 By SmallAcademy Pt. 1-3 begin*/
         val queue = Volley.newRequestQueue(this.context)
-        val url = "https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformake/${brandData}?format=json"
+        val url = "https://api.api-ninjas.com/v1/cars?limit=30&make=${brandData}"
 
-        val modelRequest = JsonObjectRequest(
-            Request.Method.GET, url, null, { response ->
-                val results = response.getJSONArray("Results")
-                Log.d("User-ModelCars", "Info: $results")
-                if(response.length() == 0){
-                    Log.d("SUCCES-ModelCars", "If-statement == 0")
-                }else{
-                    Log.d("SUCCES-ModelCars", "If-statement != 0")
-                    for (car in 0..(results.length()-1)){
-                        val objRes = results[car].toString()
-                        val gsonConverter = Gson()
-                        val modelCar = gsonConverter.fromJson(objRes, ModelCar::class.java)
+        val modelRequest: JsonArrayRequest = object: JsonArrayRequest(
+            Request.Method.GET, url, null, Response.Listener { response ->
+                Log.d("User-ModelCars", "Info: $response")
+                for (car in 0 until response.length()){
+                    val objRes = response.getJSONObject(car)
+                    val carBrandName = objRes.getString("make")
+                    val carModelName = objRes.getString("model")
 
-                        Log.d("User-Model", "Info: ${modelCar.Model_Name}")
-                        carsModelsList.add(modelCar)
-                    }
+                    carsModelsList.add(ModelCar(carBrandName, carModelName))
                 }
 
                 val recyclerView: RecyclerView = binding.recyclerView
                 recyclerView.layoutManager = LinearLayoutManager(this.context)
                 recyclerView.adapter = ModelCarAdapter(carsModelsList, this)
-            }, { Log.d("User-Error-Model", "Something went wrong") })
+
+            }, Response.ErrorListener{ Log.d("User-Error-Model", "Something went wrong") })
+        {
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String> {
+                val params = HashMap<String, String>()
+                params["X-Api-Key"] = api_key
+                return params
+            }
+        }
 
         queue.add(modelRequest)
         /*https://www.youtube.com/watch?v=e3MDW87mbR8 By SmallAcademy Pt. 1-3 eind*/
@@ -73,8 +80,8 @@ class ModelCarsFragment : Fragment(), ModelCarAdapter.OnItemClickListener {
     }
 
     override fun onItemClick(position: Int) {
-        val brand = carsModelsList.get(position).Make_Name
-        val model = carsModelsList.get(position).Model_Name
+        val brand = carsModelsList.get(position).make
+        val model = carsModelsList.get(position).model
         /*https://gist.github.com/codinginflow/a05784462aa8e2a60961539ecd803110 By Coding in Flow begin*/
         val args = Bundle()
         args.putString(ARG_NAME, brand.lowercase())
