e3fcf1f3d618a13182ecc6291510283ac205dd60
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-05-29 23:38:34 +0200

    Oplossing probleem API22
    Files in drawable-v24 i.p.v. drawable

--- Autaxion/app/src/main/java/com/hanneloremaes/autaxion/MainActivity.kt
+++ Autaxion/app/src/main/java/com/hanneloremaes/autaxion/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.hanneloremaes.autaxion.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -34,7 +35,7 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
             )
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        setupActionBarWithNavController(navController, appBarConfiguration )
         navView.setupWithNavController(navController)
     }
 }
