36dcfc7a1098f4c606e104cc8cda0edb2676dda2
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-05-29 18:56:04 +0200

    Het zetten van toolbar -> HomeActivity

--- Autaxion/app/src/main/java/com/hanneloremaes/autaxion/HomeDetailCar.kt
+++ Autaxion/app/src/main/java/com/hanneloremaes/autaxion/HomeDetailCar.kt
@@ -1,28 +1,35 @@
 package com.hanneloremaes.autaxion
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.ImageButton
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.hanneloremaes.autaxion.ui.home.HomeFragment
+import kotlinx.android.synthetic.main.activity_home_detail_car.*
 
 class HomeDetailCar : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_detail_car)
 
+        setSupportActionBar(findViewById(R.id.detail_toolbar))
+
         /*https://www.youtube.com/watch?v=fp-b9o4HgTg&list=PLhHQ6SXLVn4vNSg0QtLPxj0Q2uj0hvKQ1&index=5 By MS Pengejar begin*/
-        val btnBack: ImageButton = findViewById(R.id.backBtn)
-        btnBack.setOnClickListener {
+
+        val textName: TextView = findViewById(R.id.nameFragment)
+        textName.text = "Home"
+
+        backBtnActivity.setOnClickListener {
             val homeFragment= HomeFragment()
             val fragment: Fragment? = supportFragmentManager.findFragmentByTag(HomeFragment::class.java.simpleName)
 
             if (fragment !is HomeFragment){
                 supportFragmentManager.beginTransaction().add(R.id.homeFragmentContainer, homeFragment, HomeFragment::class.java.simpleName).commit()
             }
-            btnBack.visibility = View.GONE
+            backBtnActivity.visibility = View.GONE
         }
         /*https://www.youtube.com/watch?v=fp-b9o4HgTg&list=PLhHQ6SXLVn4vNSg0QtLPxj0Q2uj0hvKQ1&index=5 By MS Pengejar eind*/
     }
