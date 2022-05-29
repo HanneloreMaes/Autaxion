4c2e63449817b13db8611f70c3180dd50ae9a2ef
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-05-29 20:21:57 +0200

    String meegeven als textwaarde -> HomeDetailFragment

--- Autaxion/app/src/main/java/com/hanneloremaes/autaxion/HomeDetailCar.kt
+++ Autaxion/app/src/main/java/com/hanneloremaes/autaxion/HomeDetailCar.kt
@@ -1,6 +1,5 @@
 package com.hanneloremaes.autaxion
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -10,7 +9,6 @@ import com.hanneloremaes.autaxion.ui.home.HomeFragment
 import kotlinx.android.synthetic.main.activity_home_detail_car.*
 
 class HomeDetailCar : AppCompatActivity() {
-    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_detail_car)
@@ -20,7 +18,7 @@ class HomeDetailCar : AppCompatActivity() {
         /*https://www.youtube.com/watch?v=fp-b9o4HgTg&list=PLhHQ6SXLVn4vNSg0QtLPxj0Q2uj0hvKQ1&index=5 By MS Pengejar begin*/
 
         val textName: TextView = findViewById(R.id.nameFragment)
-        textName.text = "Home"
+        textName.text = getString(R.string.title_home)
 
         backBtnActivity.setOnClickListener {
             val homeFragment= HomeFragment()
